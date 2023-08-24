33e3ec8689db41557af8e9e645f2f7c4a36b34f6
Artur <129121513+ArturMaia@users.noreply.github.com>
2023-08-24 20:16:39 -0300

    Add files via upload

---
+++ tabuada.py
@@ -0,0 +1,8 @@
+numero= int(input('digite um numero:'))
+contador=0
+
+while contador <=9:
+    contador +=1
+    calculo = numero * contador
+    print(f'{numero}x{contador}= {calculo}')
+print('estude!')
